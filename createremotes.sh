10786d3449bf8fa5437e1b672cd7c082b58bb551
Dimitris Katikaridis <dkatikaridis@gmail.com>
2020-03-20 20:28:22 +0200

    createremotes.sh: Support the new manifest layout

--- createremotes.sh
+++ createremotes.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+ROOT_DIR="$(pwd)"
 for entry in "$(pwd)"/src/*
 do
     if [ -d "$entry" ]; then #if "$entry is a directory"
@@ -9,4 +10,22 @@ do
         cd ../..
     fi
 done
-echo "Remotes added."
+echo "Project's remotes added."
+
+# manifest
+cd $ROOT_DIR/manifest
+git remote remove origin > /dev/null 2>&1
+git remote add origin git@github.com/ifdroids/manifest.git
+cd ..
+# resources
+cd $ROOT_DIR/resources
+git remote remove origin > /dev/null 2>&1
+git remote add origin git@github.com/ifdroids/resources.git
+cd ..
+# terminal_scripts
+cd $ROOT_DIR/terminal_scripts
+git remote remove origin > /dev/null 2>&1
+git remote add origin git@github.com/ifdroids/terminal_scripts.git
+cd ..
+
+echo "All remotes added."
