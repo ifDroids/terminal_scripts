39d453837d561e5d9639abd05ed093843cefca87
Dimitris Katikaridis <dkatikaridis@gmail.com>
2020-03-20 20:09:52 +0200

    createremotes: Correctly silence fatal error when no origin exists

--- createremotes.sh
+++ createremotes.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-
-for entry in "$(pwd)"/*
+for entry in "$(pwd)"/src/*
 do
     if [ -d "$entry" ]; then #if "$entry is a directory"
-        cd src/"$entry"
+        cd "$entry"
         y="${entry##*/}"
-        git remote remove origin >> /dev/null
+        git remote remove origin > /dev/null 2>&1
         git remote add origin git@github.com/ifdroids/"$y".git
         cd ../..
     fi
